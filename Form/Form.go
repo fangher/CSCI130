2f5a26d384bccd09a8d55d1ad53aef71cc5eff40
fangher <fangher@users.noreply.github.com>
2015-03-18 10:40:23 -0700

    Form

--- Form/Form.go
+++ Form/Form.go
@@ -47,13 +47,3 @@ func showResults(w http.ResponseWriter, r *http.Request) {
   }
 }
 
-// Get the Port from the environment so we can run on Heroku
-func GetPort() string {
-  var port = os.Getenv("PORT")
-// Set a default port if there is nothing in the environment
-  if port == "" {
-    port = "4747"
-    fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-  }
-  return ":" + port
-}
