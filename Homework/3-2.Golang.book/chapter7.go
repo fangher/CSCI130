65be234ed8e61f0dec7fd9eacb6292f95938253c
fangher <fangher@users.noreply.github.com>
2015-05-04 14:06:26 -0700

    Golang Chpater 7

---
+++ Homework/3-2.Golang.book/chapter7.go
@@ -0,0 +1,43 @@
+package main
+
+func halfCheck(myHalfBool float64) bool {
+	myHalfBool = myHalfBool / 2
+	boolValue := false
+	if math.Mod(myHalfBool, 2) == 0 {
+		boolValue = true
+	}
+	return boolValue
+}
+
+func gList(myArgList ...int) int {
+	greatest := 0
+	for _, v := range myArgList {
+		if greatest < v {
+			greatest = v
+		}
+	}
+	return greatest
+}
+
+func MOG() func() uint {
+	i := uint(1)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+func Recfib(fibInt int) int {
+	if fibInt == 0 || fibInt == 1 {
+		return fibInt
+	} else {
+		return (Recfib(fibInt-1) + Recfib(fibInt-2))
+	}
+}
+
+func swapVal(x, y *int) {
+	temp := *x
+	*x = *y
+	*y = temp
+}
